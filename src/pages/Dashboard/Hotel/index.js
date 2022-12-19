71530c2dc73a90c7bb550fb30fd3bc8dbd240544
Igor Melo <igormeloigormelo@gmail.com>
2022-12-19 17:25:45 -0300

    feat: accomodation types: user can see the accomodation types of the hotels

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -2,13 +2,11 @@ import { useEffect } from 'react';
 import {
   BookingPage,
   Title,
-  LabelMessage,
-  Hotels,
-  HotelContainer
+  LabelMessage
 } from './styles';
 
 import useTicket from '../../../hooks/api/useTicket';
-import useHotel from '../../../hooks/api/useHotel';
+import { HotelComponent } from '../../../components/HotelComponents/HotelComponent';
 
 export default function Hotel() {
   const { ticket, getTicket } = useTicket();
@@ -25,36 +23,7 @@ export default function Hotel() {
         (!ticket.TicketType.includesHotel) ?
           <LabelMessage>Sua modalidade de ingresso não inclui hospedagem<br/>Prossiga para a escolha de atividades</LabelMessage>
           :
-          <HotelsComponent />
-    );
-  };
-
-  const HotelsComponent = () => {
-    const { hotel, getHotel } = useHotel();
-
-    useEffect(async() => {
-      await getHotel();
-    }, []);
-
-    return (
-      <Hotels>
-        <p>Primeiro, escolha seu hotel</p>
-
-        {hotel ? 
-          hotel.map(hotel => <HotelComponent hotel={hotel} />)
-          :
-          <></>
-        }
-      </Hotels>
-    );
-  };
-
-  const HotelComponent = ({ hotel }) => {
-    return (
-      <HotelContainer>
-        <img src={hotel.image} />
-        <p>{hotel.name}</p>
-      </HotelContainer>
+          <HotelComponent />
     );
   };
 
--- src/services/hotelApi.js
+++ src/services/hotelApi.js
@@ -19,4 +19,14 @@ export async function getHotelList(token) {
 
   return response.data;
 }
+
+export async function getHotelWithRooms(token, hotelId) {
+  const response = await api.get(`/hotels/${hotelId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
 //
