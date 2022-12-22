3dd8d3ce33cb3496db3ae7aa42b887c4c89c6c55
vinicius <vinicius.coassis@gmail.com>
2022-12-22 09:14:54 -0300

    feat: warn message when ticket isnt payed

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,52 @@
+import { Typography } from '@material-ui/core';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import styled from 'styled-components';
+import useEvent from '../../../hooks/api/useEvent';
+import useTicket from '../../../hooks/api/useTicket';
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const { ticket } = useTicket();
+  const { event } = useEvent();
+  const [ userTicket, setUserTicket] = useState([]);
+  const [ isRemote, setIsRemote ] = useState([]);
+  const [ isPayed, setIsPayed ] = useState(false);
+
+  useEffect(async() => {
+    const userTicket = await ticket();
+    setUserTicket(userTicket);
+    if(userTicket.TicketType.isRemote) {
+      setIsRemote(true);
+    }
+    if(userTicket.status === 'PAID') {
+      setIsPayed(true);
+    }
+  }, []);
+
+  return (
+    <>
+      {isPayed? 
+        isRemote? 
+          <WarnMessage>Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades.</WarnMessage> :
+          <WarnMessage>PAGO</WarnMessage> : 
+        <WarnMessage>Você precisa ter confirmado pagamento antes
+        de fazer a escolha de atividades</WarnMessage>
+      }
+     
+    </>
+  );
 }
+
+const WarnMessage = styled.div`
+  width: 80%;
+  height: 100%;
+  transform: translateX(10%);
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  z-index: 1;
+  font-size: 1.2rem;
+  color: gray;
+  font-weight: lighter;
+
+`;
