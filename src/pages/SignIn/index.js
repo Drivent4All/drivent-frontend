d9796cd82d71adfd9dca6dfc6d94dc2ed66d06a6
Igor Melo <igormeloigormelo@gmail.com>
2023-01-04 18:02:24 -0300

    feat: oauth: button to login with github

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,22 +8,48 @@ import Input from '../../components/Form/Input';
 import Button from '../../components/Form/Button';
 import Link from '../../components/Link';
 import { Row, Title, Label } from '../../components/Auth';
+import { GithubButtonComponent } from '../../components/GithubButtonComponent';
 
 import EventInfoContext from '../../contexts/EventInfoContext';
 import UserContext from '../../contexts/UserContext';
 
 import useSignIn from '../../hooks/api/useSignIn';
+import useOAuth from '../../hooks/api/useOAuth';
+
+const CLIENT_ID = '0b7cd7b14981961d3711';
 
 export default function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const { loadingSignIn, signIn } = useSignIn();
+  const { loadingOAuthLogin, oauthSignIn } = useOAuth();
 
   const { eventInfo } = useContext(EventInfoContext);
   const { setUserData } = useContext(UserContext);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get('code');
+
+    if(codeParam !== null) {
+      signInWithGithub(codeParam);
+    }
+  }, []);
+
+  async function signInWithGithub(code) {
+    try {
+      const userData = await oauthSignIn(code);
+      setUserData(userData);
+      toast('Login realizado com sucesso!');
+      navigate('/dashboard');
+    } catch (err) {
+      toast('Não foi possível fazer o login!');
+    }
+  }
   
   async function submit(event) {
     event.preventDefault();
@@ -38,6 +64,10 @@ export default function SignIn() {
     }
   } 
 
+  function loginWithGithub() {
+    window.location.assign('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID);
+  }
+
   return (
     <AuthLayout background={eventInfo.backgroundImageUrl}>
       <Row>
@@ -49,8 +79,9 @@ export default function SignIn() {
         <form onSubmit={submit}>
           <Input label="E-mail" type="text" fullWidth value={email} onChange={e => setEmail(e.target.value)} />
           <Input label="Senha" type="password" fullWidth value={password} onChange={e => setPassword(e.target.value)} />
-          <Button type="submit" color="primary" fullWidth disabled={loadingSignIn}>Entrar</Button>
+          <Button type="submit" color="primary" fullWidth disabled={loadingSignIn || loadingOAuthLogin}>Entrar</Button>
         </form>
+        <GithubButtonComponent onClick={loginWithGithub} />
       </Row>
       <Row>
         <Link to="/enroll">Não possui login? Inscreva-se</Link>
